33758cf5b05acda9fea530c457c372fca511539d
Maledor <josh.r.cameron@gmail.com>
2017-01-20 20:17:12 -0500

    set up server to send static file and stated writing html file "

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 var express = require('express');
 var server = express();
 
-var port = 
+var port = 8080
+
+server.get('/', function(request, response){
+  response.sendFile('index.html', {root: __dirname + '/public/html'})
+});
+server.listen(port, function(){
+  console.log('Now listening on port', port);
+});
